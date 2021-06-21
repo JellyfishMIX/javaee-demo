9e2c954eee2841b681aa3b34263095d5f84eb72e
qianshijie <2273516339@qq.com>
2021-06-21 10:03:02 +0800

    修改了 /cookie/home servlet 的 doGet 方法

--- src/main/java/servlet/cookie/HomeServlet.java
+++ src/main/java/servlet/cookie/HomeServlet.java
@@ -15,46 +15,6 @@ import java.io.PrintWriter;
  */
 @WebServlet("/cookie/home")
 public class HomeServlet extends HttpServlet {
-    // public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    //     // 防止字符集乱码
-    //     response.setContentType("text/html;charset=utf-8");
-    //     String username = "";
-    //     String checked = "";
-    //
-    //     // 需要在登录时，将用户信息存入到Cookie中，在下次登录时可以从Cookie文件中读取这个用户之前保存的用户信息
-    //     // 1. 在登录时，先取出之前存好的Cookie
-    //     Cookie[] cookies = request.getCookies();
-    //     if (cookies != null) {
-    //         for (Cookie cookie : cookies) {
-    //             if ("SaveUser".equals(cookie.getName())) {
-    //                 // 说明这个cookie存在
-    //                 username = cookie.getValue();
-    //                 // username不为null，也不为空串
-    //                 if (username != null && !username.equals("")) {
-    //                     // username有值，就一定要改checked属性
-    //                     checked = "checked = 'checked'";
-    //                 }
-    //                 // 找到后跳出循环
-    //                 break;
-    //             }
-    //         }
-    //
-    //         // 返回 response
-    //         // 标签输出，就是为了输出一个表单。
-    //         // 为什么不用页面，因为页面中写不了Java代码
-    //         // 具体代码参见《D:\河北农大\Java EE\09 cookie&session\资料\资料类\LoginServlet.txt》
-    //         PrintWriter out = response.getWriter();
-    //         // form表单
-    //         out.write("<form action='" + request.getContextPath() + "/cookie/login' method='post'>");
-    //         // html标签
-    //         out.write("用户名：<input type='text' name='username' value='" + username + "' /><br/>");
-    //         out.write("密码：<input type='password' name='password'/><br/>");
-    //         out.write("记住用户名：<input type='checkbox' value='true' name='remember' " + checked + "/><br/>");
-    //         out.write("<input type='submit' value='登录'/><br/>");
-    //         out.write("</form>");
-    //     }
-    // }
-
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // 防止字符集乱码
         response.setContentType("text/html;charset=utf-8");
